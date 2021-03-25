8f4673a1a9c3a303818aa135867466b53be3d8d2
satomakoto0 <satomakotosat0mak0k0t0satomakoto@gmail.com>
2021-03-25 18:28:20 +0900

    Change models/Posts.js

--- models/Posts.js
+++ models/Posts.js
@@ -13,7 +13,10 @@ const postSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
   },
-});
+},
+  {
+    timestamps: true
+  });
 
 const post = mongoose.model('post', postSchema);
 
